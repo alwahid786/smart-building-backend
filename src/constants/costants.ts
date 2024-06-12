f8bcd53b2660142660abb0cb2b2396063bf0bbb3
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-06-12 18:20:58 +0500

    Delete all files folder

--- src/constants/costants.ts
+++
@@ -1,5 +0,0 @@
-import path from 'node:path';
-import { fileURLToPath } from 'url';
-
-export const __dirName = fileURLToPath(import.meta.url);
-export const __fileName = path.dirname(__dirName);
--- src/routes/index.routes.ts
+++
@@ -1,8 +0,0 @@
-import { authRoutes } from "./authRoutes/auth.routes.js"
-
-
-export const allApiRoutes = (app:any)=>{
-
-    // auth routes
-    authRoutes(app);
-}
