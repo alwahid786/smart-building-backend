493680cccb2a9fded40fb285381353b4aa077fb6
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-06-26 17:53:07 +0500

    change server url

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ export const app = express();
 // middleware
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["https://smart-building-frontend-z9w2.vercel.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
