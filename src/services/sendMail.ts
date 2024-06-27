6d2d6db966059a7fab5b72da4d1af4df1f3d054e
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-06-27 11:28:39 +0500

    change nodemailer transporter

--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -2,20 +2,20 @@ import nodemailer from "nodemailer";
 import { config } from "../config/config.js";
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
+  host: 'smtp.titan.email',
   port: 587,
   auth: {
-      user: 'gaetano.collier@ethereal.email',
-      pass: 'K7HsCDJwQJfwrDCy7y'
+      user: 'development@tetratechnologies.io',
+      pass: 'ZIm DE~.r9lD>PF'
   }
 });
 
 export const sendMail = async (to: string, subject: string, text: string) => {
     try {
-        const from = config.getEnv("NODEMAILER_USER");
+        const from ="development@tetratechnologies.io";
         if (!from || !to || !subject || !text) throw new Error("Please Provide To, Subject and Text");
         const myTransPorter = transporter;
-        await myTransPorter.sendMail({ from, to, subject, text});
+        await myTransPorter.sendMail({ from:`"Tetra Technologies" <${from}>`, to, subject, text});
         return true;
     } catch (error) {
         return false;
