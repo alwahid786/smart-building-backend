fb75154ef267d2ca839c7b6c98105ccf28dbd895
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-07-13 19:10:12 +0500

    change

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://smart-building-frontend-gi2r-k4yxkn2mc-wahid-ahmads-projects.vercel.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
