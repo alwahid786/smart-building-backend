923a3c42319080726f27032fa23556f6df5544b0
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-07-30 05:24:13 -0700

    change production configuration url

--- src/app.ts
+++ src/app.ts
@@ -17,8 +17,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        // origin: ["http://localhost:5173","https://smart-building-frontend-xp24gu3vc-wahid-ahmads-projects.vercel.app"],
-        origin: ["http://localhost:5173"],
+        origin: ["https://smart-building-frontend-xp24gu3vc-wahid-ahmads-projects.vercel.app"],
+        // origin: ["http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
