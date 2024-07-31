f3f2f71f73154d929dd98aafca7257600602a3cc
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-07-30 22:31:48 -0700

    update production to local corse origin url

--- src/app.ts
+++ src/app.ts
@@ -17,8 +17,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ["https://smart-building-frontend-ym94-n1wgm80lx-wahid-ahmads-projects.vercel.app"],
-        // origin: ["http://localhost:5173"],
+        // origin: ["https://smart-building-frontend-ym94-n1wgm80lx-wahid-ahmads-projects.vercel.app"],
+        origin: ["http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
