fc21954900d790e9f5e8aff9e5b760d5d2820440
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-08-02 18:36:19 +0500

    change frontend config url

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ["https://smart-building-frontend-ym94-n1wgm80lx-wahid-ahmads-projects.vercel.app"],
+        origin: ["https://smart-building-frontend-wjgt.vercel.app"],
         // origin: ["http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
